59aa8a41b08cf277e5866f84f8ead1e20ecf43b2
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-05-09 17:09:54 +0800

    contactPage dutu

--- frontend/mergejil/src/App.jsx
+++ frontend/mergejil/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/navbar";
 import Instructor from "./pages/InstracturPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InformationSidebar from "./components/InformationSidebar";
+import ContactPage from "./pages/ContactPage";
+import PageHeadersNavbar from "./components/PageHeadersNavbar";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Navbar />}></Route>
           <Route path="/Instructor" element={<Instructor />}></Route>
           <Route path="/footer" element={<InformationSidebar />}></Route>
+          <Route path="/contact" element={<ContactPage />}></Route>
+          <Route path="/header" element={<PageHeadersNavbar />}></Route>
         </Routes>
       </BrowserRouter>
     </>
