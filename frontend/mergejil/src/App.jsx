d448cdf293ac2f2f3a387d3f53d96d400f56fcd0
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-05-16 16:48:45 +0800

    contact page duussn

--- frontend/mergejil/src/App.jsx
+++ frontend/mergejil/src/App.jsx
@@ -6,33 +6,27 @@ import InformationSidebar from "./components/InformationSidebar";
 import ContactPage from "./pages/ContactPage";
 import PageHeadersNavbar from "./components/PageHeadersNavbar";
 import HomeInformation from "./components/homeInformation";
-
 import ProfessionPage from "./pages/ProfessionPage";
 import MergjeilHeader from "./components/mergejilHeader";
 
 function App() {
   return (
     <>
-      
       <BrowserRouter>
         <Routes>
-
-          <Route path="/" element={<AboutUsIntroduce />}></Route>
+          {/* <Route path="/" element={<AboutUsIntroduce />}></Route> */}
           <Route path="/Instructor" element={<Instructor />}></Route>
           <Route path="/footer" element={<InformationSidebar />}></Route>
           <Route path="/contact" element={<ContactPage />}></Route>
           <Route path="/header" element={<PageHeadersNavbar />}></Route>
-          <Route path="/aboutOwnPage" element={<AboutUsOwnPage />}></Route>
+          {/* <Route path="/aboutOwnPage" element={<AboutUsOwnPage />}></Route> */}
 
           <Route path="/" element={<ProfessionPage />}></Route>
           <Route path="/Instructor" element={<Instructor />}></Route>
-          <Route path="/SignUp" element={<SignUp />}></Route>
-          <Route path="/Course" element={<CoursePage />}></Route>
-
+          {/* <Route path="/SignUp" element={<SignUp />}></Route> */}
+          {/* <Route path="/Course" element={<CoursePage />}></Route> */}
         </Routes>
       </BrowserRouter>
-
-    
     </>
   );
 }
